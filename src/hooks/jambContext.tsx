7af38fe5762526f8977018a3b545524798d10d4e
BashBuilder <iamadelowotan@gmail.com>
2024-04-09 20:57:07 +0100

    lit question update

--- src/hooks/jambContext.tsx
+++ src/hooks/jambContext.tsx
@@ -71,7 +71,7 @@ const JambProvider: React.FC<ProviderChildrenProps> = ({ children }) => {
             literatureSnapshot.forEach((doc) => lit.push(doc.data()));
             literatureData = {
               subject: "literature",
-              data: lit[1].data,
+              data: lit[2].data,
             };
           } else {
             url = `https://questions.aloc.com.ng/api/v2/m/40?subject=${subject}&type=utme`;
